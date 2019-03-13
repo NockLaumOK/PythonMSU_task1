a0dd7720180b308e190eee7abf74863587ea905b
NockLaumOK <nocklaumok@gmail.com>
2019-03-13 14:40:14 +0300

    Task 1_2

--- example.py
+++ example.py
@@ -8,6 +8,10 @@ root = Tk()
 
 Butt = Button(root, command=fnB,  text="Butt ON")
 Butt.grid()
+Butt1 = Button(root, command=fnB,  text="Butt ON")
+Butt1.grid()
+Butt2 = Button(root, command=fnB,  text="Butt ON")
+Butt2.grid()
 
 root.mainloop()
 print("QQ") #prints after closing
